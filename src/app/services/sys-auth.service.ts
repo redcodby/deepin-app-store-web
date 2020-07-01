cec520db475195a696776832bdad0a6c8effe10a
wuhao <wuhkcc@gmail.com>
2020-07-01 17:39:48 +0800

    fix: issue with notification when intranetAuthState is not true
    添加了一个枚举类型IntranetExceedLimit,
    终端数量超过限制时给出对应的提示信息

--- src/app/services/sys-auth.service.ts
+++ src/app/services/sys-auth.service.ts
@@ -3,28 +3,47 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SysAuthService {
   noIntranetAuth$ = new BehaviorSubject(null);
-
+  intranetAuthNotifyType: NotifyType;
   constructor() {
     this.getIntranetAuthState();
-   }
+  }
 
   async getIntranetAuthState() {
-    const resp = await Channel.exec<any>('settings.getIntranetAuthState');
-    const intranetAuthState = resp === true;
-    this.noIntranetAuth$.next(intranetAuthState);
+    try {
+      const resp = await Channel.exec<any>('settings.getIntranetAuthState');
+      const result = JSON.parse(resp);
+      // 注册状态
+      const intranetAuthState = result.is_register === true;
+      if (intranetAuthState !== true) {
+        this.configIntranetAuthNotifyType(result);
+      }
+      this.noIntranetAuth$.next(intranetAuthState);
+    } catch(e) {
+      console.log('err', e);
+      this.intranetAuthNotifyType = NotifyType.IntranetNotAuthorized;
+      this.noIntranetAuth$.next(false);
+    }
   }
 
   notifyIntranetFail() {
-    Channel.exec('account.authorizationNotify', NotifyTpye.IntranetNotAuthorized);
+    Channel.exec('account.authorizationNotify', this.intranetAuthNotifyType);
   }
 
+  configIntranetAuthNotifyType(result: any = {}) {
+    if (result.code === 40005) {
+      this.intranetAuthNotifyType = NotifyType.IntranetExceedLimit;
+    } else {
+      this.intranetAuthNotifyType = NotifyType.IntranetNotAuthorized;
+    }
+  }
 }
 
-enum NotifyTpye {
+enum NotifyType {
   ExtranetNotAuthorized,
   IntranetNotAuthorized,
+  IntranetExceedLimit,
 }
